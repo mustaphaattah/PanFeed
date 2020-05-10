a85e6088e09d90a02d33b8652bdc8a7f9fc5e7c3
mustaphaattah <mustaphaattah@gmail.com>
2020-05-10 17:19:05 -0400

    swipe refresh on global fragment recycler view

--- app/src/main/java/com/mtah/panfeed/GlobalFragment.kt
+++ app/src/main/java/com/mtah/panfeed/GlobalFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.mtah.panfeed.models.News
 import retrofit2.Call
 import retrofit2.Callback
@@ -25,7 +26,10 @@ class GlobalFragment : Fragment() {
     val TAG = "GlobalFragment"
     val COVID_KEYWORD = "coronavirus"
     val SORT_BY = "publishedAt"
-    val PAGE_SIZE = 100
+    private val PAGE_SIZE = 100
+    var recyclerView: RecyclerView? = null
+    var swipeRefresh: SwipeRefreshLayout? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,32 +37,46 @@ class GlobalFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_global, container, false)
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.globalRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView = view.findViewById(R.id.globalRecyclerView)
+        recyclerView!!.layoutManager = LinearLayoutManager(activity)
 
-        var request = NewsApiClient.getApi(NewsInterface::class.java)
-        var lang = getLanguange()
-        var call = request.getAllCovidNews(getString(R.string.api_key), COVID_KEYWORD, lang, PAGE_SIZE, SORT_BY)
+        swipeRefresh = view.findViewById(R.id.globalRefresh)
+        swipeRefresh!!.setOnRefreshListener { fetchNews() }
+
+        //fill the recycler view with news cards
+        fetchNews()
+
+        return view
+    }
+
+    private fun fetchNews(){
+        swipeRefresh!!.isRefreshing = true
+        Log.d(TAG, "fetching News")
+        val request = NewsApiClient.getApi(NewsInterface::class.java)
+        val lang = getLanguage()
+        val call = request.getAllCovidNews(getString(R.string.api_key), COVID_KEYWORD, lang, PAGE_SIZE, SORT_BY)
         Log.d(TAG, "news language $lang")
 
         Log.d(TAG, "Sending requests...")
         call.enqueue(object : Callback<News> {
             override fun onFailure(call: Call<News>, t: Throwable) {
+                swipeRefresh!!.isRefreshing = false
                 Log.e(TAG, "Response Failure")
                 Toast.makeText(activity, t.localizedMessage, Toast.LENGTH_SHORT)
                 Log.e(TAG, t.localizedMessage)
             }
 
             override fun onResponse(call: Call<News>, response: Response<News>) {
+                swipeRefresh!!.isRefreshing = false
                 if (response.isSuccessful) {
                     Log.d(TAG, "onResponse successful: Showing articles")
 
-                    recyclerView.apply {
+                    recyclerView!!.apply {
                         //layoutManager = LinearLayoutManager(this@HomeActivity)
                         itemAnimator = DefaultItemAnimator()
                         adapter = NewsAdapter(response.body()!!.articles, context)
 
-                        var newsCount = (adapter as NewsAdapter).itemCount
+                        val newsCount = (adapter as NewsAdapter).itemCount
                         Log.d(TAG, "onResponse successful:  Done! got $newsCount articles")
                         if (newsCount == 0)
                             Toast.makeText(activity, "No Global news to show", Toast.LENGTH_LONG)
@@ -68,12 +86,9 @@ class GlobalFragment : Fragment() {
             }
         })
 
-
-
-        return view
     }
 
-    fun getLanguange() : String{
+    private fun getLanguage() : String{
         return Locale.getDefault().language
     }
 }
