579556a88d0612bfb1ae1d6ba6f01ea0ffc4d579
mustaphaattah <mustaphaattah@gmail.com>
2020-09-22 16:59:37 -0400

    added test ad banner and internet connection check in MainActivity.kt.

--- app/src/main/java/com/mtah/panfeed/MainActivity.kt
+++ app/src/main/java/com/mtah/panfeed/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.mtah.panfeed
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.mtah.panfeed.fragments.CasesFragment
 import com.mtah.panfeed.fragments.NewsFragment
 import com.mtah.panfeed.fragments.SavedFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val fragments: ArrayList<Fragment> = arrayListOf(NewsFragment(), CasesFragment(), SavedFragment())
-    var fragmentNumber: Int = 0
+    private var fragmentNumber: Int = 0
     private var currentFragment = fragments[0]
-    private val TAG = "MainActivity"
+    private val savedKey = "index"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,35 +30,47 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolBar)
         setSupportActionBar(toolbar)
 
-        //set default view to news fragment
-        if (savedInstanceState != null) {
-            Log.i(TAG, "onCreate: savedInstanceState = ${savedInstanceState.getInt("index")}")
-            fragmentNumber = savedInstanceState.getInt("index")
-            Log.i(TAG, "onCreate: fragment Number = $fragmentNumber")
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragments[fragmentNumber])
-                .commit()
-            currentFragment = fragments[fragmentNumber]
-        } else {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, NewsFragment())
-                .commit()
-            fragmentNumber = fragments.indexOf(currentFragment)
+        val connectionSnackbar = Snackbar.make( findViewById(R.id.mainActivityLayout),
+            "Oops No internet, check your connection.",
+            Snackbar.LENGTH_INDEFINITE)
+            .setAction("RETRY") {
+                recreate()
+            }
+        //Mobile Ads
+        if (!isConnectedToInternet(this.applicationContext)){
+            connectionSnackbar.show()
         }
+        else {
+            MobileAds.initialize(this) {}
+            adView.loadAd(AdRequest.Builder().build())
 
-        //bottom navigation bar
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationListener)
 
+            //set default view to news fragment
+            if (savedInstanceState != null) {
+                fragmentNumber = savedInstanceState.getInt(savedKey)
+                Log.i(TAG, "onCreate: fragment Number = $fragmentNumber")
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, fragments[fragmentNumber])
+                    .commit()
+                currentFragment = fragments[fragmentNumber]
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, NewsFragment())
+                    .commit()
+                fragmentNumber = fragments.indexOf(currentFragment)
+            }
+
+            //bottom navigation bar
+            val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+            bottomNavigation.setOnNavigationItemSelectedListener(navigationListener)
+        }
     }
 
     //bottom navigation bar Listener
     private val navigationListener =  BottomNavigationView.OnNavigationItemSelectedListener{ item ->
         var selectedFragment: Fragment = NewsFragment()
         when (item.itemId) {
-            R.id.nav_news -> {
-                selectedFragment = fragments[0]
-            }
+            R.id.nav_news -> selectedFragment = fragments[0]
             R.id.nav_cases -> selectedFragment =  fragments[1]
             R.id.nav_saved -> selectedFragment =  fragments[2]
         }
@@ -65,16 +83,23 @@ class MainActivity : AppCompatActivity() {
         true
     }
 
+    private fun isConnectedToInternet(context: Context): Boolean {
+        val connectivityManager: ConnectivityManager = context
+            .getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        return networkInfo != null && networkInfo.isConnectedOrConnecting
+    }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        Log.i(TAG, "onRestoreInstanceState: fragmentNumber = $fragmentNumber")
-        fragmentNumber = savedInstanceState.getInt("index")
+        fragmentNumber = savedInstanceState.getInt(savedKey)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        Log.i(TAG, "onSaveInstanceState: current Fragment = $fragmentNumber")
-        outState.putInt("index", fragments.indexOf(currentFragment))
+        outState.putInt(savedKey, fragments.indexOf(currentFragment))
     }
 
     companion object {
@@ -82,6 +107,7 @@ class MainActivity : AppCompatActivity() {
         const val EXTRA_URL = "com.mtah.panfeed.URL"
         const val EXTRA_IMAGE_URL = "com.mtah.panfeed.IMAGEURL"
         const val EXTRA_DATE = "com.mtah.panfeed.DATE"
+        private const val TAG = "MainActivity"
     }
 
 }
--- app/src/main/java/com/mtah/panfeed/fragments/NewsFragment.kt
+++ app/src/main/java/com/mtah/panfeed/fragments/NewsFragment.kt
@@ -1,6 +1,8 @@
 package com.mtah.panfeed.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyCharacterMap
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,35 +16,53 @@ import com.mtah.panfeed.adapters.PagerAdapter
 
 class NewsFragment : Fragment() {
 
+    private val KEY = "position"
+    private val TAG = "NewsFragment"
+    lateinit var fragments: ArrayList<Fragment>
+    var currentFragment = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-         val view = inflater.inflate(R.layout.fragment_news, container, false)
+        val view = inflater.inflate(R.layout.fragment_news, container, false)
+
+        fragments = arrayListOf(GlobalFragment(), LocalFragment())
 
 
         val tabLayout: TabLayout = view.findViewById(R.id.tabLayout)
         val viewPager: ViewPager = view.findViewById(R.id.viewPager)
 
-//        tabLayout.addTab(tabLayout.newTab().setText("Global"))
-//        tabLayout.addTab(tabLayout.newTab().setText("Local"))
-
-//        val adapter = PagerAdapter(childFragmentManager, tabLayout.tabCount)
         val pagerAdapter = PagerAdapter(childFragmentManager)
         pagerAdapter.addFragment("Global", GlobalFragment())
         pagerAdapter.addFragment("Local", LocalFragment())
 
         viewPager.adapter = pagerAdapter
+        if (savedInstanceState != null) {
+            currentFragment = savedInstanceState.getInt(KEY)
+            viewPager.currentItem = currentFragment
+        }
 
         tabLayout.setupWithViewPager(viewPager)
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                viewPager.currentItem = tab!!.position
+                var position = tab?.position
+                if (position != null) {
+                    viewPager.currentItem = position
+                    currentFragment = viewPager.currentItem
+                    Log.i(TAG, "onTabSelected: current Tab === $position")
+                }
+
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
-                viewPager.currentItem = tab!!.position
+                var position = tab?.position
+                if (position != null) {
+                    viewPager.currentItem = position
+                    currentFragment = viewPager.currentItem
+                    Log.i(TAG, "onTabSelected: current Tab === $position")
+                }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
@@ -50,5 +70,17 @@ class NewsFragment : Fragment() {
         })
 
         return view
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY, currentFragment)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        if (savedInstanceState != null) {
+            currentFragment = savedInstanceState.getInt(KEY)
+        }
     }
 }
